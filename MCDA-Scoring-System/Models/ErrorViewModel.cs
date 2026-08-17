9a07010de7eb4d90d1e492a25eda3f741f8325d8
admin <admin@MY-PC-2>
2026-08-17 10:06:20 +0300

    Add project files.

---
+++ MCDA-Scoring-System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MCDA_Scoring_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
