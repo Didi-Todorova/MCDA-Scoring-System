ad677095f3dec6e9c52bf74755ee4ae1ddc7e85a
Didi Todorova <didi100504@abv.bg>
2026-08-27 13:50:41 +0300

    Restucturing Project

---
+++ MCDA-Scoring-System/MCDA-Scoring-System.Infrastructure/DependencyInjection/MCDAServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace MCDA_Scoring_System.MCDA_Scoring_System.Infrastructure.DependencyInjection
+{
+    public static class MCDAServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // Register your application services here
+            // Example: services.AddScoped<IMyService, MyService>();
+            return services;
+        }
+    }
+}
